baa74d7405ac3ce989987673dee6b9ea52cdd584
kinstugi <kinstugi.webdev@gmail.com>
2026-09-01 05:19:05 +0200

    clean up later

---
+++ email_detection_layer/detector.py
@@ -0,0 +1,38 @@
+import imaplib
+from email import policy
+from email.parser import BytesParser
+from dotenv import load_dotenv
+import os
+
+
+def establish_connection() -> (bool, any):
+    try:
+        imap = imaplib.IMAP4_SSL("imap.gmail.com", 993)
+    except:
+        return False, None
+    return True, imap
+
+
+def fetch_email(imap, email_id):
+    status, data = imap.fetch(email_id, "(RFC822)")
+    raw_data = data[0][1]
+    msg = BytesParser(policy=policy.default).parsebytes(raw_data)
+    print(msg)
+    
+
+def check_new_emails(imap):
+    pass
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    did_conn, imap_conn = establish_connection()
+    gmail_pass = os.getenv("GMAIL_PASS")
+    
+    if did_conn:
+        res, val = imap_conn.login("kinstugi.webdev@gmail.com", gmail_pass)
+        imap_conn.select("INBOX")
+        stat, unread_mail_bytes = imap_conn.search(None, "UNSEEN")
+        unread_email = unread_mail_bytes[0].split()
+        fetch_email(imap_conn, unread_email[-1])
+        imap_conn.logout()

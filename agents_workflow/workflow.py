67fac447f015d3a12c67ecb35f4102c82ed0e40f
kinstugi <kinstugi.webdev@gmail.com>
2026-09-15 11:17:39 +0200

    feat: update job status handling and enhance email processing logic

--- agents_workflow/workflow.py
+++ agents_workflow/workflow.py
@@ -1,4 +1,5 @@
-from db_contexts.repos.email_repository import get_queued_jobs_by_stat
+from db_contexts.repos.email_repository import get_queued_jobs_by_stat, update_queued_job_status
+from db_contexts.models.email_retriever_models import JobStatus
 from .agents.classifier import get_classifying_agent
 from .agents.extractor_agent import get_extractor_agent
 import asyncio
@@ -17,8 +18,13 @@ async def main():
         file_path = Path.joinpath(Path.cwd(), "data", "emails", f"{job.email_id}_email.json")
         email_data = json.loads(file_path.read_text())
         res = await classifier.run(email_data.get('content'))
+        print(res.output)
+
         if not res.output.is_quote:
+            update_queued_job_status(job.id, JobStatus.COMPLETED)
+            file_path.unlink()
             continue
+        update_queued_job_status(job.id, JobStatus.CLASSIFIED)
         res = await extractor.run(email_data.get('content'))
         print(res)
         break
